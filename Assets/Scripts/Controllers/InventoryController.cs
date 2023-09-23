9e27a98f133f3e661550bb2a301505c3ec1f56b2
NewRound <86642886+NewRound@users.noreply.github.com>
2023-09-23 17:53:16 +0900

    플레이어 정보 표시 수정중

--- Assets/Scripts/Controllers/InventoryController.cs
+++ Assets/Scripts/Controllers/InventoryController.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    GameManager gameManager = GameManager.instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -6,6 +6,11 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [Header("GameObject")]
+    [SerializeField] GameObject player;
+
+    [Header("UI")]
+    [SerializeField] GameObject BackgroundUICanvas;
     [SerializeField] GameObject selectUICanvas;
     [SerializeField] GameObject playerInfoUICanvas;
     [SerializeField] GameObject InventoryUICanvas;
